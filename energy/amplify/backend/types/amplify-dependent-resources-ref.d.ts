44b7cf47f0b9ca3bb321ef86bcf92e7fc84cc1b9
Sanketh <sanketh.nair@energymanagers.uk>
2025-04-20 16:39:52 +0530

    initial commit

---
+++ energy/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "energymanager2c223c87": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
